4ac88528437782e2d9a4ed6799b850b258d9c714
Mitchell Scott <mitchellscott@me.com>
2022-02-05 20:58:49 -0600

    dbcontext set up for success with identitycore

--- src/Data/Entities/Users.cs
+++ src/Data/Entities/Users.cs
@@ -8,6 +8,7 @@ namespace CashTrack.Data.Entities
     public class Users : IdentityUser<int>, IEntity
     {
         new public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
-
 }
