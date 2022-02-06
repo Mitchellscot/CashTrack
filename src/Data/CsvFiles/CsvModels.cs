9da4d53a5aa4a4032fb16010b4be8b7ab7c799f6
Mitchell Scott <mitchellscott@me.com>
2022-02-05 21:42:34 -0600

    db is pretty clean now

--- src/Data/CsvFiles/CsvModels.cs
+++ src/Data/CsvFiles/CsvModels.cs
@@ -84,10 +84,11 @@ namespace CashTrack.Data.CsvFiles
         public class CsvUser
         {
             public int Id { get; set; }
-            public string? first_name { get; set; }
-            public string? last_name { get; set; }
-            public string? email { get; set; }
-            public string? password_hash { get; set; }
+            public string? UserName { get; set; }
+            public string? FirstName { get; set; }
+            public string? LastName { get; set; }
+            public string? Email { get; set; }
+            public string? PasswordHash { get; set; }
         }
     }
 }
