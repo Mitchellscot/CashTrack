4bfde29569cdbad6d82867945267655bce640833
Mitchell Scott <mitchellscott@me.com>
2022-02-11 22:09:26 -0600

    layout for index is done

--- src/Pages/Login.cshtml.cs
+++ src/Pages/Login.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace CashTrack.Pages
 {
@@ -12,5 +13,12 @@ namespace CashTrack.Pages
         public void OnGet()
         {
         }
+        //public async Task<IActionResult> OnPostAsync()
+        //{
+        //    if (!ModelState.IsValid)
+        //        return Page();
+
+        //    return Page();
+        //}
     }
 }
