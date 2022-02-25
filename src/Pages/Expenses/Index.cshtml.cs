fa20a50f86e6eb8b0244e3eddadf1085b4036a9c
Mitchell Scott <mitchellscott@me.com>
2022-02-24 21:28:25 -0600

    wip

--- src/Pages/Expenses/Index.cshtml.cs
+++ src/Pages/Expenses/Index.cshtml.cs
@@ -1,16 +1,34 @@
 using CashTrack.Models.Common;
+using CashTrack.Models.ExpenseModels;
+using CashTrack.Services.ExpenseService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace CashTrack.Pages.Expenses
 {
     public class Index : PageModel
     {
+        private readonly IExpenseService _service;
+
+        public Index(IExpenseService service) => _service = service;
+
         [BindProperty]
         public DateOptions QueryType { get; set; }
-        public void OnGet()
+        [BindProperty]
+        public ExpenseRequest ExpenseRequest { get; set; }
+        public ExpenseResponse ExpenseResponse { get; set; }
+
+        public async Task<ActionResult> OnGet()
         {
+            ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.All });
+            return Page();
+        }
+        public async Task<IActionResult> OnGetPresetQuery(DateOptions options)
+        {
+            var request = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = options });
+            return Page();
         }
     }
 }
