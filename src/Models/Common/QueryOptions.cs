f3872e7345e42d51650f78093927e31570e969a3
Mitchell Scott <mitchellscott@me.com>
2022-03-04 17:14:47 -0600

    added some jquery

---
+++ src/Models/Common/QueryOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CashTrack.Models.Common
+{
+    public enum QueryOptions
+    {
+        Date,
+        
+        Amount,
+        Merchant,
+        [Display(Name="Sub Category")]
+        SubCategory,
+        [Display(Name = "Main Category")]
+        MainCategory,
+        Tag
+    }
+}
+
