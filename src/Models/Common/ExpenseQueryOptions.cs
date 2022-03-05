0987fc05b5d9a2602c556599aa91d9414d3bfc0f
Mitchell Scott <mitchellscott@me.com>
2022-03-05 14:59:28 -0600

    significant changes to expense table

---
+++ src/Models/Common/ExpenseQueryOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CashTrack.Models.Common
+{
+    public static class ExpenseQueryOptions
+    {
+        public static readonly Dictionary<int, string> GetAll = new Dictionary<int, string>()
+        {
+            { 0, "Date" },
+            { 1, "Date Range" },
+            { 2, "Month" },
+            { 3, "Quarter" },
+            { 4, "Year" },
+            { 5, "Amount" },
+            { 6, "Notes" },
+            { 7, "Merchant" },
+            { 8, "Sub Category" },
+            { 9, "Main Category" },
+            { 10, "Tag" }
+        };
+    }
+}
