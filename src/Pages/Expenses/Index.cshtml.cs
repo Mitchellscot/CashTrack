40fe6bbbb2163e730a8bb7e8188a9e93bdb39403
Mitchell Scott <mitchellscott@me.com>
2022-03-04 22:53:26 -0600

    form improvements

--- src/Pages/Expenses/Index.cshtml.cs
+++ src/Pages/Expenses/Index.cshtml.cs
@@ -18,22 +18,96 @@ namespace CashTrack.Pages.Expenses
         [BindProperty]
         public string q { get; set; }
         [BindProperty]
+        public string q2 { get; set; }
+        [BindProperty]
         public QueryOptions query { get; set; }
         [BindProperty]
         public ExpenseResponse ExpenseResponse { get; set; }
 
-
-        public async Task<ActionResult> OnGet(string q, QueryOptions query)
+        public async Task<ActionResult> OnGet(string q, QueryOptions query, string q2)
         {
             if (query == QueryOptions.Date && q != null)
             {
-                if (!DateTime.TryParse(q, out _))
-                {
-                    ModelState.AddModelError("", "Date is not formatted properly");
-                    return Page();
-
-                }
                 ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.SpecificDate, BeginDate = DateTime.Parse(q) });
+                return Page();
+            }
+            if (query == QueryOptions.DateRange && q != null && q2 != null)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.DateRange, BeginDate = DateTime.Parse(q), EndDate = DateTime.Parse(q2) });
+                return Page();
+            }
+            if (query == QueryOptions.Month && q != null)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.SpecificMonthAndYear, BeginDate = DateTime.Parse(q) });
+                return Page();
+            }
+            if (query == QueryOptions.Quarter && q != null)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.SpecificQuarter, BeginDate = DateTime.Parse(q) });
+                return Page();
+            }
+            if (query == QueryOptions.Year && q != null)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.SpecificYear, BeginDate = DateTime.Parse(q) });
+                return Page();
+            }
+            if (query == QueryOptions.CurrentMonth)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.CurrentMonth });
+                return Page();
+            }
+            if (query == QueryOptions.CurrentQuarter)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.CurrentQuarter });
+                return Page();
+            }
+            if (query == QueryOptions.CurrentYear)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.CurrentYear });
+                return Page();
+            }
+            if (query == QueryOptions.Last30Days)
+            {
+                ExpenseResponse = await _service.GetExpensesAsync(new ExpenseRequest() { DateOptions = DateOptions.Last30Days });
+                return Page();
+            }
+            if (query == QueryOptions.Amount)
+            {
+                decimal amount;
+                if (Decimal.TryParse(q, out amount))
+                {
+                    ExpenseResponse = await _service.GetExpensesByAmountAsync(new AmountSearchRequest() { Query = amount });
+                    return Page();
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Unable to convert the given amount. Please try again.");
+                    return Page();
+                }
+            }
+            if (query == QueryOptions.Notes)
+            {
+                ExpenseResponse = await _service.GetExpensesByNotesAsync(new ExpenseRequest() { Query = q });
+                return Page();
+            }
+            if (query == QueryOptions.Merchant)
+            {
+                ModelState.AddModelError("", "Not Implemented Yet");
+                return Page();
+            }
+            if (query == QueryOptions.SubCategory)
+            {
+                ModelState.AddModelError("", "Not Implemented Yet");
+                return Page();
+            }
+            if (query == QueryOptions.MainCategory)
+            {
+                ModelState.AddModelError("", "Not Implemented Yet");
+                return Page();
+            }
+            if (query == QueryOptions.Tag)
+            {
+                ModelState.AddModelError("", "Not Implemented Yet");
                 return Page();
             }
 
