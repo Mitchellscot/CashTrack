d2cb032f97a23569bfdeda9410325d6796b8cec0
Mitchell Scott <mitchellscott@me.com>
2022-03-14 08:48:02 -0500

    about to create a split expense page

---
+++ src/Pages/Expenses/Split.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CashTrack.Pages.Expenses
+{
+    public class SplitModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
