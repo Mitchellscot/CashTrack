a90bafa5f4d526211e0e86ca8f76627132feec1e
Mitchell Scott <mitchellscott@me.com>
2022-03-19 10:40:32 -0500

    WIP

--- src/Pages/Income/Refund.cshtml.cs
+++ src/Pages/Income/Refund.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CashTrack.Pages.Incomes
@@ -24,11 +25,33 @@ namespace CashTrack.Pages.Incomes
         [BindProperty]
         public string Query { get; set; }
         public CashTrack.Models.IncomeModels.Income Income { get; set; }
+        [BindProperty]
         public Expense[] ExpenseSearch { get; set; }
         public List<Expense> SelectedExpenses { get; set; }
+        [BindProperty]
+        public List<int> ExpenseSearchChosenIds { get; set; } = new();
 
         public async Task<IActionResult> OnGet(int id, string Query)
         {
+            Income = await _incomeService.GetIncomeByIdAsync(id);
+            Total = Income.Amount;
+            return Page();
+        }
+        public async Task<IActionResult> OnPostQuery(List<int> expenseIds, string Query, int incomeId)
+        {
+            //when a search is made,
+            //Post the query so we can get the expense search results
+            //Each row in the expense search table is a small form that posts the individual id
+            //and when that posts, it sends the ID to the other POST method
+            //which adds the id to a list, which then rewrites the form to include that id
+            //so a list of ids builds up in the expense query table
+            //and the selected query table at the same time
+            //that way you can do a search, add an expense, do a search, add an expense, and so on until the
+            //selected expense table is full of the amount of expenses you need
+            //after that you post to another handler method that
+            //processes all the refunds
+            //
+            //kind of complicated, but it's just managing state across two different forms
             if (Query != null)
             {
                 DateTime q;
@@ -40,14 +63,17 @@ namespace CashTrack.Pages.Incomes
                 ExpenseSearch = await _expenseService.GetExpensesByDateWithoutPaginationAsync(q);
             }
 
+            if (expenseIds != null)
+                ExpenseSearchChosenIds.AddRange(expenseIds);
 
-            Income = await _incomeService.GetIncomeByIdAsync(id);
-            Total = Income.Amount;
             return Page();
         }
-        public async Task<IActionResult> OnPost(List<int> expenseIds, string Query)
+        public async Task<IActionResult> OnPost(int ExpenseSearchId, string Query)
         {
-            foreach (var id in expenseIds)
+            if (ExpenseSearchId > 0)
+                SelectedExpenses = new();
+
+            foreach (var id in ExpenseSearchChosenIds)
             {
                 var selectedExpense = await _expenseService.GetExpenseByIdAsync(id);
                 SelectedExpenses.Add(selectedExpense);
