0e84f7009a24f437e0bb0029d525f825f68940a2
Mitchell Scott <mitchellscott@me.com>
2022-03-23 17:15:24 -0500

    set up a table

---
+++ src/Pages/Review/Income.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CashTrack.Pages.Review
+{
+    public class IncomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
