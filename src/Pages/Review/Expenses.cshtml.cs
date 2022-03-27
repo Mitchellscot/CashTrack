fd1e68cb55a5258bb40a9ca67883e0fb273ed8d2
Mitchell Scott <mitchellscott@me.com>
2022-03-27 18:35:11 -0500

    added a dropdown list for expense category (needs cleanup)

--- src/Pages/Review/Expenses.cshtml.cs
+++ src/Pages/Review/Expenses.cshtml.cs
@@ -1,10 +1,12 @@
 using CashTrack.Models.ExpenseReviewModels;
+using CashTrack.Models.SubCategoryModels;
 using CashTrack.Services.ExpenseReviewService;
+using CashTrack.Services.SubCategoryService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.PowerShell;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Threading.Tasks;
@@ -13,10 +15,12 @@ namespace CashTrack.Pages.Review
 {
     public class ExpensesModel : PageModel
     {
+        private readonly ISubCategoryService _subCategoryService;
         private readonly IExpenseReviewService _expenseService;
 
-        public ExpensesModel(IExpenseReviewService expenseService)
+        public ExpensesModel(IExpenseReviewService expenseService, ISubCategoryService subCategoryService)
         {
+            _subCategoryService = subCategoryService;
             _expenseService = expenseService;
         }
         [TempData]
@@ -24,11 +28,12 @@ namespace CashTrack.Pages.Review
         public ExpenseReviewResponse ExpenseReviewResponse { get; set; }
         [BindProperty(SupportsGet = true)]
         public int PageNumber { get; set; } = 1;
-        public async Task<IActionResult> OnGet(int pageNumber)
+        [BindProperty]
+        //public SelectList SuggestedCategory { get; set; }
+        public SubCategoryDropdownSelection[] CategoryList { get; set; }
+        public async Task<IActionResult> OnGet()
         {
-            PageNumber = ExpenseReviewResponse != null ? ExpenseReviewResponse.PageNumber : pageNumber == 0 ? 1 : pageNumber;
-
-            ExpenseReviewResponse = await _expenseService.GetExpenseReviewsAsync(new ExpenseReviewRequest() { PageNumber = PageNumber });
+            await PrepareData();
             return Page();
         }
         public async Task<IActionResult> OnPostRunScript()
@@ -49,10 +54,15 @@ namespace CashTrack.Pages.Review
                 ps.Commands.AddScript(@". C:\Users\Mitch\Code\CashTrack\ct-data\Scripts\Run-Transactions.ps1");
                 var result = await ps.InvokeAsync().ConfigureAwait(false);
             }
+            await PrepareData();
+            return Page();
+        }
+        private async Task PrepareData()
+        {
+            CategoryList = await _subCategoryService.GetSubCategoryDropdownListAsync();
             PageNumber = ExpenseReviewResponse != null ? ExpenseReviewResponse.PageNumber : PageNumber == 0 ? 1 : PageNumber;
 
             ExpenseReviewResponse = await _expenseService.GetExpenseReviewsAsync(new ExpenseReviewRequest() { PageNumber = PageNumber });
-            return Page();
         }
     }
 }
