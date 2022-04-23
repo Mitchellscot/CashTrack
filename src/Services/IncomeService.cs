dc54e722fc0b2b3985c095938a010f56ff5d8332
Mitchell Scott <mitchellscott@me.com>
2022-04-23 12:19:46 -0500

    added link to source from income list page

--- src/Services/IncomeService.cs
+++ src/Services/IncomeService.cs
@@ -154,6 +154,7 @@ public class IncomeMapperProfile : Profile
             .ForMember(x => x.Amount, o => o.MapFrom(x => x.Amount))
             .ForMember(x => x.Category, o => o.MapFrom(x => x.Category.Name))
             .ForMember(x => x.Source, o => o.MapFrom(x => x.Source.Name))
+            .ForMember(x => x.SourceId, o => o.NullSubstitute(null))
             .ForMember(x => x.Notes, o => o.MapFrom(x => x.Notes))
             .ForMember(x => x.IsRefund, o => o.MapFrom(x => x.IsRefund))
             .ForMember(x => x.RefundNotes, o => o.MapFrom(x => x.RefundNotes));
