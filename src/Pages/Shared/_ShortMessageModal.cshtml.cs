67d9432833e2dbb65235a63cbd128e9b027badf1
Mitchell Scott <mitchellscott@me.com>
2022-05-29 11:59:12 -0500

    fixed an annoying bug with the message partial

--- src/Pages/Shared/_ShortMessageModal.cshtml.cs
+++ src/Pages/Shared/_ShortMessageModal.cshtml.cs
@@ -1,11 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace CashTrack.Pages.Shared
 {
-    public class _ShortMessageModal : PageModel
+    public class _ShortMessageModal
     {
-        public string Id { get; set; }
         public string Title { get; set; }
         public string Message { get; set; }
     }
