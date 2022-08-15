eacb5533b85dd31aae828d53491b0f28168c09b1
Mitchell Scott <mitchellscott@me.com>
2022-08-15 13:42:04 -0500

    appsettings class was ignored by git

---
+++ src/Common/AppSettingsOptions.cs
@@ -0,0 +1,14 @@
+﻿using CashTrack.Data.Entities;
+using System.Collections.Generic;
+
+namespace CashTrack.Common
+{
+    public class AppSettingsOptions
+    {
+        public const string AppSettings = "AppSettings";
+        public string Secret { get; set; }
+        public string CsvFileDirectory { get; set; }
+        public KeyValuePair<string, string> ConnectionStrings { get; set; }
+        public UserEntity[] Users { get; set; }
+    }
+}
