5343cef62fa6712ca6f886d1fa72fbd79bb7bb96
Mitchell Scott <mitchellscott@me.com>
2022-08-18 08:39:51 -0500

    edited test

--- tests/CashTrack.Tests/Controllers/ControllerTests.cs
+++ tests/CashTrack.Tests/Controllers/ControllerTests.cs
@@ -1,12 +1,18 @@
 ﻿using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
+using CashTrack.Models.IncomeCategoryModels;
+using CashTrack.Models.MainCategoryModels;
+using CashTrack.Models.SubCategoryModels;
 using CashTrack.Tests.Common;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Shouldly;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -14,7 +20,7 @@ using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace CashTrack.Tests.Web;
+namespace CashTrack.Tests.Controllers;
 
 public class ControllerTests : IClassFixture<CustomWebApplicationFactory<CashTrack.Program>>
 {
@@ -28,28 +34,101 @@ public class ControllerTests : IClassFixture<CustomWebApplicationFactory<CashTra
         _factory = factory;
         _client = GetAuthenticatedClient();
     }
-    [Theory]
-    [InlineData("/Expenses")]
-    [InlineData("/Expenses/Categories")]
-    [InlineData("/Merchants")]
-    [InlineData("/Merchants/Detail/1")]
-    [InlineData("/Expenses/Categories/Detail/1")]
-    [InlineData("/Expenses/Categories/Main")]
-    [InlineData("/Expenses/Split/1")]
-    [InlineData("/Income")]
-    [InlineData("/Sources/Detail/1")]
-    [InlineData("/Income/Refund/1")]
-    [InlineData("/Settings")]
-    public async Task Can_Return_Secure_Pages(string path)
+    [Fact]
+    public async Task Income_Category_Controller_Returns_Categories()
     {
+        var response = await _client.GetAsync("/api/IncomeCategory");
 
-        var response = await _client.GetAsync(path);
-
-        var html = await response.Content.ReadAsStringAsync();
-        response.Content.Headers.ContentType!.ToString().ShouldBe("text/html; charset=utf-8");
+        var result = await response.Content.ReadAsStringAsync();
+        var responseObject = JsonConvert.DeserializeObject<IncomeCategoryDropdownSelection[]>(result);
+        responseObject.Length.ShouldBe(10);
+        responseObject.Last().Category.ShouldBe("Bonus");
         response.StatusCode.ShouldBe(HttpStatusCode.OK);
-        PrintRequestAndResponse(path, html);
+        PrintRequestAndResponse("/api/IncomeCategory", result);
+    }
+    [Fact]
+    public async Task Sub_Category_Controller_Returns_Categories()
+    {
+        var response = await _client.GetAsync("/api/SubCategory");
 
+        var result = await response.Content.ReadAsStringAsync();
+        var responseObject = JsonConvert.DeserializeObject<SubCategoryDropdownSelection[]>(result);
+        responseObject.Length.ShouldBe(31);
+        responseObject.Last().Category.ShouldBe("Travel Misc");
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        PrintRequestAndResponse("/api/SubCategory", result);
+    }
+    [Fact]
+    public async Task Main_Category_Controller_Returns_Categories()
+    {
+        var response = await _client.GetAsync("/api/MainCategory");
+
+        var result = await response.Content.ReadAsStringAsync();
+        var responseObject = JsonConvert.DeserializeObject<MainCategoryDropdownSelection[]>(result);
+        responseObject.Length.ShouldBe(16);
+        responseObject.Last().Category.ShouldBe("Vacation");
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        PrintRequestAndResponse("/api/MainCategory", result);
+    }
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+
+    public async Task Main_Category_Controller_Returns_By_Subcategory_Id(int id)
+    {
+        var response = await _client.GetAsync($"/api/maincategory/sub-category/{id}");
+
+        var result = await response.Content.ReadAsStringAsync();
+        result.ShouldNotBeEmpty();
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        PrintRequestAndResponse("/api/maincategory/sub-category/", result);
+    }
+    [Theory]
+    [InlineData("BZT Tips")]
+    [InlineData("Alliance Redwoods")]
+    [InlineData("SCT Tips")]
+
+    public async Task Income_Source_Controller_Returns_Matching_Sources(string name)
+    {
+        var response = await _client.GetAsync($"/api/incomesource?sourceName={name}");
+
+        var result = await response.Content.ReadAsStringAsync();
+        var responseObject = JsonConvert.DeserializeObject<string[]>(result);
+        responseObject[0].ShouldBe(name);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        PrintRequestAndResponse("/api/IncomeSource?sourceName=", result);
+    }
+    [Theory]
+    [InlineData("Groceries")]
+    [InlineData("Hobbies")]
+    [InlineData("Rent")]
+
+    public async Task Subcategory_Controller_Returns_Matching_Categories(string name)
+    {
+        var response = await _client.GetAsync($"/api/subcategory/autocomplete?categoryName={name}");
+
+        var result = await response.Content.ReadAsStringAsync();
+        var responseObject = JsonConvert.DeserializeObject<string[]>(result);
+        responseObject[0].ShouldBe(name);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        PrintRequestAndResponse("/api/subcategory/autocomplete?categoryName=", result);
+    }
+    [Theory]
+    [InlineData("Costco")]
+    [InlineData("In N Out")]
+    [InlineData("Target")]
+
+    public async Task Merchants_Controller_Returns_Matching_Merchants(string name)
+    {
+        var response = await _client.GetAsync($"/api/merchants?merchantName={name}");
+
+        var result = await response.Content.ReadAsStringAsync();
+        var responseObject = JsonConvert.DeserializeObject<string[]>(result);
+        responseObject.Length.ShouldBe(1);
+        responseObject[0].ShouldContain(name);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        PrintRequestAndResponse("/api/merchants?merchantName=", result);
     }
     private HttpClient GetAuthenticatedClient()
     {
