f6403e04f5338d47b4d7d9549c441afb645ca6b2
Mitchell Scott <57727600+Mitchellscot@users.noreply.github.com>
2022-08-19 11:15:58 -0500

    Feature/aggregator tests (#34)
    * added unit tests
    
    * added csv parser tests
    
    * edited test file

---
+++ tests/CashTrack.Tests/Unit/CsvParserTests.cs
@@ -0,0 +1,71 @@
+﻿using CashTrack.Data.CsvFiles;
+using Shouldly;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace CashTrack.Tests.Unit
+{
+    public class CsvParserTests
+    {
+        private readonly string _csvFileDirectory;
+        public CsvParserTests()
+        {
+            _csvFileDirectory = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.Parent!.Parent!.Parent!.FullName, "ct-data", "TestData");
+        }
+        [Fact]
+        public void Can_Parse_Users_File()
+        {
+            var result = CsvParser.ProcessUserFile(Path.Combine(_csvFileDirectory, "Users.csv"));
+            result.FirstOrDefault()!.FirstName.ShouldBe("Test");
+        }
+        [Fact]
+        public void Can_Parse_Main_Categories_File()
+        {
+            var result = CsvParser.ProcessMainCategoryFile(Path.Combine(_csvFileDirectory, "MainCategories.csv"));
+            result.Count().ShouldBe(16);
+        }
+        [Fact]
+        public void Can_Parse_Sub_Category_File()
+        {
+            var result = CsvParser.ProcessSubCategoryFile(Path.Combine(_csvFileDirectory, "SubCategories.csv"));
+            result.Count().ShouldBe(37);
+        }
+        [Fact]
+        public void Can_Parse_Merchants_File()
+        {
+            var result = CsvParser.ProcessMerchantFile(Path.Combine(_csvFileDirectory, "Merchants.csv"));
+            result.Count().ShouldBe(16);
+        }
+        [Fact]
+        public void Can_Parse_Expenses_File()
+        {
+            var result = CsvParser.ProcessExpenseFile(Path.Combine(_csvFileDirectory, "Expenses.csv"));
+            result.Count().ShouldBe(462);
+        }
+        [Fact]
+        public void Can_Parse_Income_Category_File()
+        {
+            var result = CsvParser.ProcessIncomeCategoryFile(Path.Combine(_csvFileDirectory, "IncomeCategories.csv"));
+            result.Count().ShouldBe(10);
+        }
+        [Fact]
+        public void Can_Parse_Source_File()
+        {
+            var result = CsvParser.ProcessIncomeSourceFile(Path.Combine(_csvFileDirectory, "IncomeSources.csv"));
+            result.Count().ShouldBe(14);
+        }
+        [Fact]
+        public void Can_Parse_Income_File()
+        {
+            var result = CsvParser.ProcessIncomeFile(Path.Combine(_csvFileDirectory, "Income.csv"));
+            result.Count().ShouldBe(225);
+        }
+        [Fact]
+        public void Can_Parse_Import_Rule_File()
+        {
+            var result = CsvParser.ProcessImportRuleFile(Path.Combine(_csvFileDirectory, "ImportRules.csv"));
+            result.Count().ShouldBe(57);
+        }
+    }
+}
---
+++ tests/CashTrack.Tests/Unit/TotalsAggregatorTests.cs
@@ -0,0 +1,45 @@
+﻿using CashTrack.Data;
+using CashTrack.Data.Entities;
+using CashTrack.Models.IncomeModels;
+using CashTrack.Pages.Import;
+using CashTrack.Repositories.ExpenseRepository;
+using CashTrack.Services.Common;
+using CashTrack.Tests.Services.Common;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CashTrack.Tests.Unit
+{
+    public class TotalsAggregatorTests
+    {
+        private List<ExpenseEntity> _expenses;
+
+        public TotalsAggregatorTests()
+        {
+            var db = new AppDbContextFactory().CreateDbContext();
+            var expenseRepo = new ExpenseRepository(db);
+            _expenses = expenseRepo.Find(x => true).Result.ToList();
+        }
+
+        [Fact]
+        public void Totals_Aggregator_Works()
+        {
+            var newExpense = new ExpenseEntity() { Amount = 1, Date = DateTime.Today, CategoryId = 1 };
+            _expenses.Add(newExpense);
+            var result = _expenses.Aggregate(new TotalsAggregator<ExpenseEntity>(),
+                (acc, e) => acc.Accumulate(e),
+                acc => acc.Compute());
+            result.Average.ShouldBe(68.13m);
+            result.Min.ShouldBe(1);
+            result.Max.ShouldBe(1600);
+            result.Count.ShouldBe(463);
+            result.TotalSpentAllTime.ShouldBe(31544.403M);
+            result.TotalSpentThisMonth.ShouldBe(1);
+            result.TotalSpentThisYear.ShouldBe(1);
+        }
+    }
+}
