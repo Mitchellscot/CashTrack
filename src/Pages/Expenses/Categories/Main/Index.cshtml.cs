15e921c5d162d7176c7e75c0e1c371a5e72760e1
Mitchell Scott <mitchellscott@me.com>
2022-10-03 22:24:00 -0500

    wip

--- src/Pages/Expenses/Categories/Main/Index.cshtml.cs
+++ src/Pages/Expenses/Categories/Main/Index.cshtml.cs
@@ -1,13 +1,34 @@
+using CashTrack.Common.Exceptions;
+using CashTrack.Models.MainCategoryModels;
+using CashTrack.Models.SubCategoryModels;
 using CashTrack.Pages.Shared;
+using CashTrack.Services.MainCategoriesService;
+using CashTrack.Services.SubCategoryService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Threading.Tasks;
 
 namespace CashTrack.Pages.Expenses.Categories.Main
 {
     public class IndexModel : PageModelBase
     {
-        public void OnGet()
+        private readonly IMainCategoriesService _mainCategoryService;
+        public MainCategoryResponse MainCategoryResponse { get; set; }
+
+        public IndexModel(IMainCategoriesService mainCategoryService)
         {
+            _mainCategoryService = mainCategoryService;
+        }
+        public async Task<ActionResult> OnGetAsync()
+        {
+            return await PrepareAndRenderPage();
+        }
+
+        private async Task<ActionResult> PrepareAndRenderPage()
+        {
+            MainCategoryResponse = await _mainCategoryService.GetMainCategoriesAsync(new MainCategoryRequest() {  });
+            return Page();
         }
     }
 }
