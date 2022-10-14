848b3daa2a639d06c584b5ca666c410ce8e082d6
Mitchell Scott <mitchellscott@me.com>
2022-10-13 19:11:14 -0500

    added cool radio buttons

--- src/Pages/Expenses/Categories/Main/Index.cshtml.cs
+++ src/Pages/Expenses/Categories/Main/Index.cshtml.cs
@@ -3,6 +3,7 @@ using CashTrack.Models.MainCategoryModels;
 using CashTrack.Pages.Shared;
 using CashTrack.Services.MainCategoriesService;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CashTrack.Pages.Expenses.Categories.Main
@@ -12,9 +13,8 @@ namespace CashTrack.Pages.Expenses.Categories.Main
         private readonly IMainCategoriesService _mainCategoryService;
         public MainCategoryResponse MainCategoryResponse { get; set; }
         [BindProperty(SupportsGet = true)]
-        public int TimeSpan { get; set; }
-        public string Title { get; set; } = "Main Categories";
-
+        public string TimeSpan { get; set; } = "All Time";
+        public string[] TimeSpans = new[] { "All Time", "Five Years", "Three Years", "One Year", "Six Months" };
         public IndexModel(IMainCategoriesService mainCategoryService)
         {
             _mainCategoryService = mainCategoryService;
@@ -26,18 +26,10 @@ namespace CashTrack.Pages.Expenses.Categories.Main
 
         private async Task<ActionResult> PrepareAndRenderPage()
         {
-            MainCategoryResponse = await _mainCategoryService.GetMainCategoriesAsync(new MainCategoryRequest() { TimeOption = (MainCategoryTimeOptions)TimeSpan });
-            Title = DisplayTitleBasedOnTimeSpan(this.TimeSpan);
+            var timeSpanIndex = Array.IndexOf(TimeSpans, TimeSpan);
+            timeSpanIndex = timeSpanIndex < 0 || timeSpanIndex >= TimeSpans.Length ? 0 : timeSpanIndex;
+            MainCategoryResponse = await _mainCategoryService.GetMainCategoriesAsync(new MainCategoryRequest() { TimeOption = (MainCategoryTimeOptions)timeSpanIndex });
             return Page();
         }
-        private string DisplayTitleBasedOnTimeSpan(int timeSpan) => timeSpan switch
-        {
-            0 => "Showing All Expenses",
-            1 => "Expenses From the Past 5 Years",
-            2 => "Expenses From the Past 3 Years",
-            3 => "Expenses From the Year",
-            4 => "Expenses From the Past 6 Months",
-            _ => "Main Categories"
-        };
     }
 }
