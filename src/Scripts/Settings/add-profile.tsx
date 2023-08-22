3e7c50829923e1de9c06c867fa463104d8defece
Mitchell Scott <mitchellscott@me.com>
2023-08-21 23:40:07 -0500

    added react for funsies

---
+++ src/Scripts/Settings/add-profile.tsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import {useState} from 'react';
+import {createRoot} from 'react-dom/client';
+
+const container = document.getElementById('add-profile-modal');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<AddProfileModal />);
+
+
+function AddProfileModal() {
+	let [counter, setCounter] = useState(0);
+	const handleClick = () => setCounter(counter +1);
+	return (
+		<>
+			<div>
+				<button className="btn btn-primary" onClick={handleClick}>Click here</button>
+			</div>
+			<div>{counter}</div>
+		</>
+);
+}
