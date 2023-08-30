050994b63ace1d60d71120f382ffc664f25bb124
Mitchell Scott <mitchellscott@me.com>
2023-08-29 22:10:29 -0500

    wip

--- src/Scripts/Settings/add-profile.tsx
+++ src/Scripts/Settings/add-profile.tsx
@@ -11,13 +11,26 @@ function AddProfileModal() {
 	const BOTH = 'both';
 	const EXPENSE_ONLY = 'expenses-only';
 	const INCOME_ONLY = 'income-only';
-	const allQuestions = ['q2', 'q3', 'q4', 'q5', 'q6']
+	const allQuestions = ['q2', 'q3', 'q4', 'q5', 'q6', 'q7']
 	let [showIncomeInput, setShowIncomeInput] = useState(false);
 	let [amountColumnName, setAmountColumnName] = useState('Expenses');
 
+	const hideAllQuestions = () => {
+		allQuestions.forEach(x => {
+			const el = document.getElementById(x) as HTMLInputElement;
+			el.value = '';
+			el.classList.add('display-none');
+		});
+	}
+
 	const showNext = (question: Number) => {
 		const el = document.getElementById(`q${question}`);
-		el?.classList.remove('visibility-hidden');
+		el?.classList.remove('display-none');
+	}
+	const hideQuestion = (question: Number) => {
+		const el = document.getElementById(`q${question}`) as HTMLInputElement;
+		el?.classList.remove('d-flex');
+		el?.classList.add('display-none');
 	}
 	const handleContainsNegativevalue = (answer: Boolean) => {
 		const input = document.getElementById(`AddEditImportProfile.ContainsNegativeValue`) as HTMLInputElement;
@@ -25,11 +38,11 @@ function AddProfileModal() {
 		const nextQuestion = document.getElementById(nextQuestionId);
 		if (answer)
 
-			nextQuestion?.classList.remove('visibility-hidden');
+			nextQuestion?.classList.remove('display-none');
 		else
 		{
 			const hideThisInput = document.getElementById('q3') as HTMLInputElement;
-			hideThisInput.classList.add('visibility-hidden')
+			hideThisInput.classList.add('display-none')
 		}
 	}
 	const handleTransactionQuestion = (answer: string) => {
@@ -38,20 +51,19 @@ function AddProfileModal() {
 			case BOTH:
 				setShowIncomeInput(true);
 				setAmountColumnName('Expenses');
-				const q2Input = document.getElementById('q2') as HTMLInputElement;
-				q2Input.classList.add('visibility-hidden');
-				const q3Input = document.getElementById('q3') as HTMLInputElement;
-				q3Input.classList.add('visibility-hidden');
+				hideAllQuestions();
 				showNext(4);
 				break;
 			case EXPENSE_ONLY:
 				setShowIncomeInput(false);
 				setAmountColumnName('Expenses');
+				hideAllQuestions();
 				showNext(2);
 				break;
 			case INCOME_ONLY:
 				setShowIncomeInput(false);
 				setAmountColumnName('Income');
+				hideAllQuestions();
 				showNext(2);
 				const input = document.getElementById("AddEditImportProfile.DefaultTransactionType") as HTMLInputElement;
 				input.value = '1';
@@ -66,46 +78,46 @@ function AddProfileModal() {
 				<input type="hidden" name="AddEditImportProfile.NegativeValueTransactionType" id="AddEditImportProfile.NegativeValueTransactionType" value="0" />
 				<input type="hidden" name="AddEditImportProfile.DefaultTransactionType" id="AddEditImportProfile.DefaultTransactionType" value="0" />
 
-				<div className="row d-flex justify-content-center" id="q1" >
+				<div className="row" id="q1" >
 					<span className="lead mb-2">
-						Does your file contain Income, Expenses, or both?
+						Does your file contain seperate columns for income and expenses?
 					</span>
 					<div className="row mb-2">
-						<div className="form-check d-flex justify-content-center col">
-							<input className="form-check-input mx-3 lead" type="radio" id="both" name="transaction-type"
+						<div className="form-check col">
+							<input className="form-check-input mx-3" type="radio" id="both" name="transaction-type"
 								onChange={() => handleTransactionQuestion(BOTH)} />
-							<label className="form-check-label lead" htmlFor="both">
-								Both
+							<label className="form-check-label text-center" htmlFor="both">
+								Two Columns
 							</label>
 						</div>
-						<div className="form-check d-flex justify-content-center col">
-							<input className="form-check-input mx-3 lead" type="radio" name="transaction-type" id="expenses-only" onChange={() => handleTransactionQuestion(EXPENSE_ONLY)} />
-							<label className="form-check-label lead" htmlFor="expenses-only">
-								Expenses Only
+						<div className="form-check col">
+							<input className="form-check-input mx-3" type="radio" name="transaction-type" id="expenses-only" onChange={() => handleTransactionQuestion(EXPENSE_ONLY)} />
+							<label className="form-check-label text-center" htmlFor="expenses-only">
+								One (Expenses)
 							</label>
 						</div>
-						<div className="form-check d-flex justify-content-center col">
-							<input className="form-check-input mx-3 lead" type="radio" name="transaction-type" id="thirdQ"
+						<div className="form-check col">
+							<input className="form-check-input mx-3" type="radio" name="transaction-type" id="thirdQ"
 								onChange={() => handleTransactionQuestion(INCOME_ONLY)} />
-							<label className="form-check-label lead" htmlFor="thirdQ">
-								Income Only
+							<label className="form-check-label text-center" htmlFor="thirdQ">
+								One (Income)
 							</label>
 						</div>
 					</div>
 				</div>
 
-				<div className="row d-flex justify-content-center visibility-hidden" id="q2">
+				<div className="row display-none" id="q2">
 					<span className="mb-2">
 						Does your file contain negative values that indicate whether it's an expense or income?
 					</span>
 					<div className="row mb-2">
-						<div className="form-check d-flex justify-content-center col">
+						<div className="form-check  col">
 							<input className="form-check-input mx-3" type="radio" name="containsNegativeValue" onChange={() => handleContainsNegativevalue(true)} />
 							<label className="form-check-label" htmlFor="firstQ">
 								Yes
 							</label>
 						</div>
-						<div className="form-check d-flex justify-content-center col">
+						<div className="form-check col">
 							<input className="form-check-input mx-3" type="radio" name="containsNegativeValue" onChange={() => handleContainsNegativevalue(false)} />
 							<label className="form-check-label" htmlFor="secondQ">
 								No
@@ -114,18 +126,18 @@ function AddProfileModal() {
 					</div>
 				</div>
 
-				<div className="row d-flex justify-content-center visibility-hidden" id="q3">
+				<div className="row display-none" id="q3">
 					<span className="lead mb-2">
 						What does a negative value indicate? (required)
 					</span>
 					<div className="row mb-2">
-						<div className="form-check d-flex justify-content-center col">
+						<div className="form-check  col">
 							<input className="form-check-input mx-3 lead" type="radio" name="AddEditImportProfile.NegativeValueTransactionType" id="firstQ" value="0" />
 							<label className="form-check-label lead" htmlFor="firstQ">
 								Expenses are negative
 							</label>
 						</div>
-						<div className="form-check d-flex justify-content-center col">
+						<div className="form-check col">
 							<input className="form-check-input mx-3 lead" type="radio" name="AddEditImportProfile.NegativeValueTransactionType" id="secondQ" value="1" />
 							<label className="form-check-label lead" htmlFor="secondQ">
 								Income is negative
@@ -133,30 +145,29 @@ function AddProfileModal() {
 						</div>
 					</div>
 				</div>
-				<div className="row d-flex justify-content-center mb-2 visibility-hidden" id="q4">
+				<div className="row mb-2 display-none" id="q4">
 					<label htmlFor="" className="form-label lead">
 						Income Column Name (required)
 					</label>
-					<input type="text" className="form-control" name="AddEditImportProfile.IncomeColumn" />
+					<input type="text" className="form-control" name="AddEditImportProfile.IncomeColumn" onChange={() => showNext(5)} />
 				</div>
-				<div className="row d-flex justify-content-center mb-2 visibility-hidden" id="q5">
+				<div className="row mb-2 display-none" id="q5">
 					<label htmlFor="" className="form-label lead">
-						Expense Column Name (required)
+						{amountColumnName} Column Name (required)
 					</label>
-					<input type="text" className="form-control" name="AddEditImportProfile.AmountColumn" />
+					<input type="text" className="form-control" name="AddEditImportProfile.AmountColumn" onChange={() => showNext(6)} />
 				</div>
-
-				<div className="row d-flex justify-content-center mb-2 visibility-hidden" id="q6">
+				<div className="row mb-2 display-none" id="q6">
 					<label htmlFor="" className="form-label lead">
 						Date Column Name (required)
 					</label>
-					<input onChange={() => showNext(2)} type="text" className="form-control" name="AddEditImportProfile.DateColumn" />
+					<input onChange={() => showNext(7)} type="text" className="form-control" name="AddEditImportProfile.DateColumn" />
 				</div>
-				<div className="row d-flex justify-content-center mb-2 visibility-hidden" id="q7">
+				<div className="row mb-2 display-none" id="q7">
 					<label htmlFor="" className="form-label lead">
 						Transaction Notes Column Name (required)
 					</label>
-					<input onChange={() => showNext(3)} type="text" className="form-control" name="AddEditImportProfile.NotesColumn" />
+					<input type="text" className="form-control" name="AddEditImportProfile.NotesColumn" />
 				</div>
 			</form>
 		</div>
