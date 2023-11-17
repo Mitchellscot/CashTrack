4890e243d58830566154277d41e9fcafd3fc6962
Mitchell Scott <mitchellscott@me.com>
2023-11-16 21:30:47 -0600

    update dotnet-ef version

--- Prod-Db.ps1
+++ Prod-Db.ps1
@@ -1,5 +1,5 @@
 Write-Host "Installing Dotnet EF" 
-dotnet tool install --global dotnet-ef
+dotnet tool install --global dotnet-ef --version 6.0.21
 Write-Host "Creating new Migration Files" 
 dotnet ef migrations add SeededInit --no-build --project ./src/CashTrack.csproj -o ./Data/Migrations -- seed
 Write-Host "Creating new SQLite Database"
