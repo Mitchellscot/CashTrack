30f4a6bc843912f0b3e2ab0fb2160086ecfc59e3
Mitchell Scott <mitchellscott@me.com>
2025-02-21 08:56:48 -0600

    update ef

--- Prod-Db.ps1
+++ Prod-Db.ps1
@@ -1,8 +1,8 @@
 #to run this you need to have the dotnet-ef tool installed 
 #that matches the nuget package installed
 
-Write-Host "Installing Dotnet EF version 8.0.2" 
-dotnet tool install --global dotnet-ef --version 8.0.2
+Write-Host "Installing Dotnet EF version 9.0.2" 
+dotnet tool install --global dotnet-ef --version 9.0.2
 Write-Host "Creating new Migration Files" 
 dotnet ef migrations add SeededInit --no-build --project ./src/CashTrack.csproj -o ./Data/Migrations -- seed
 Write-Host "Creating new SQLite Database"
